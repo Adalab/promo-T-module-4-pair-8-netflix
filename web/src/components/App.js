c99fe9165d36d955c0d6a5e866300d3c2a18f5ce
María Sánchez <msanchezroman95@gmail.com>
2023-07-14 11:28:52 +0200

    add filters

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -40,6 +40,7 @@ const App = () => {
     };
     apiMovies.getMoviesFromApi(params).then(response => {
       setAppMovies(response.movies);
+      console.log('response.movies: ' +response.movies);
     });
   }, [allMoviesOptionGenre, allMoviesOptionSort]);
 
